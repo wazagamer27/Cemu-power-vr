6a2421a8c39c86748074121f8f36da350f9905c1
wazagamer27 <matthewasencio.q@gmail.com>
2026-08-08 20:15:49 -0500

    Add forceCompile check in VulkanPipelineCompiler
    Added a check for forceCompile before validating shader compilation.

--- src/Cafe/HW/Latte/Renderer/Vulkan/VulkanPipelineCompiler.cpp
+++ src/Cafe/HW/Latte/Renderer/Vulkan/VulkanPipelineCompiler.cpp
@@ -945,6 +945,9 @@ bool PipelineCompiler::Compile(bool forceCompile, bool isRenderThread, bool show
 	VulkanRenderer* vkRenderer = VulkanRenderer::GetInstance();
 	
 	// PowerVR compatibility check
+		
+		if (!forceCompile)
+	{
 		// fail early if some shader stages are not compiled
 		if (m_vkVertexShader && m_vkVertexShader->IsCompiled() == false)
 			return false;
